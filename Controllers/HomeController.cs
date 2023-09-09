adf95aeddbd0be4dc5d27877cee7edbdef3e0280
Yrakhwas <yrakhwas@gmail.com>
2023-09-09 11:30:16 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ASP.Net_KW_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.Net_KW_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Model()
+        {
+            return View();
+        }
+        public IActionResult Controller()
+        {
+            return View();
+        }
+        public IActionResult View() => base.View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
